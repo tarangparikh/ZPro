71f54ae09197e1f964eee31bb930d2d852b95f52
taranparikh <tp0265@gmail.com>
2019-11-18 23:40:24 +0530

    #13

--- src/main/java/dao/PersistenceDAO.java
+++ src/main/java/dao/PersistenceDAO.java
@@ -7,10 +7,10 @@ public class PersistenceDAO {
     private static final SessionFactory sessionFactory = buildSessionFactory();
     private static SessionFactory buildSessionFactory(){
         try{
-            Configuration configuration = new Configuration();
+            Configuration configuration = new Configuration().configure();
             String HEROKU_JDBC_URL = System.getenv("JDBC_DATABASE_URL");
             if(HEROKU_JDBC_URL!=null) configuration.setProperty("hibernate.connection.url",HEROKU_JDBC_URL);
-            return configuration.configure().buildSessionFactory();
+            return configuration.buildSessionFactory();
         }catch (Exception e){
             e.printStackTrace();
             return null;
