f59d4f5cd5d05ba2bc8bb2da32de99d10b9e7979
taranparikh <tp0265@gmail.com>
2019-11-19 00:07:02 +0530

    #14

--- src/main/java/dao/PersistenceDAO.java
+++ src/main/java/dao/PersistenceDAO.java
@@ -1,16 +1,29 @@
 package dao;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PersistenceDAO {
     private static final SessionFactory sessionFactory = buildSessionFactory();
     private static SessionFactory buildSessionFactory(){
         try{
             Configuration configuration = new Configuration().configure();
+            StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+            standardServiceRegistryBuilder.applySettings(configuration.getProperties());
             String HEROKU_JDBC_URL = System.getenv("JDBC_DATABASE_URL");
-            if(HEROKU_JDBC_URL!=null) configuration.setProperty("hibernate.connection.url",HEROKU_JDBC_URL);
-            return configuration.buildSessionFactory();
+            if(HEROKU_JDBC_URL!=null) {
+                Map<String,String> map = new HashMap<>();
+                map.put("hibernate.connection.url",HEROKU_JDBC_URL);
+                standardServiceRegistryBuilder.applySettings(map);
+            }
+            ServiceRegistry serviceRegistry = standardServiceRegistryBuilder.build();
+            return configuration.buildSessionFactory(serviceRegistry);
         }catch (Exception e){
             e.printStackTrace();
             return null;
